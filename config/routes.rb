c9c9bb3365fb9e84f32ee6930d797330c8a52fd9
bakikm <bakri@gmx.net>
2016-09-08 19:50:37 +0200

    mix projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'profile/index'
+
+    get 'about_me', to: 'profile#about_me'
+    get 'study_and_work', to: 'profile#study_and_work'
+    get 'my_fevorite_hobbies', to: 'profile#my_fevorite_hobbies'
+    get 'call_me', to: 'profile#call_me'
+    root 'profile#index'
+
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'posts/index'
