1ce5cda31f2d4cd685bd4b1ff05743e82c09572e
bakikm <bakri@gmx.net>
2016-09-15 20:54:42 +0200

    add reqeust

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,11 @@
 class ProfileController < ApplicationController
   def index
+    require "requests"
+
+response = Requests.request("GET", "https://www.googleapis.com/books/v1/volumes?q=inauthor:dickens")
+
+# Now you have these methods available
+response.body    #=> String with the response bod
+@data = JSON.parse(response.body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'profile/index'
 
     get 'about_me', to: 'profile#about_me'
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'posts/index'
 
-  root'posts#index'
+  root to:'posts#index'
 end
